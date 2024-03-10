3e8c87bc68b5095053f08c202965d8166959848d
DavidF2112 <160644975+DavidF2112@users.noreply.github.com>
2024-03-10 12:08:06 +0000

    6 готово

--- 07.03.24/TASK6.py
+++ 07.03.24/TASK6.py
@@ -1,1 +1,14 @@
-ne ponyal
+word = input("Enter>>  ")
+
+n = len(word)
+found = False
+for i in range(1, n//2 + 1):
+    if n % i == 0:
+        pattern = word[:i]
+        if pattern * (n//i) == word:
+            print("Repetitive word:", pattern)
+            found = True
+            break
+
+if not found:
+    print("Repetitive word not found!")
